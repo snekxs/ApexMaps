0ad0768446c2e5aaf0f976f5b71e0cd609f66565
Snekxs <snekxs@gmail.com>
2022-11-17 15:07:12 -0700

    Add Homescreen Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React  from 'react';
+import React from "react";
 import "./App.css";
-
+import Data from "./components/Data.jsx";
 
 function App() {
-	return (
-	
+	const data = Data();
+
+  return (
     <div className="App">
-      <div className="elements">Testing Word WOW</div>
-    </div>
+      <div className="elements">
+			<h1 className="time">Time Remaining: {data ? data["current"]["remainingTimer"] : "Loading..."}</h1>
+        <h1 className="currentMap">
+					Current Map: {data ? data["current"]["map"] : "Loading..."}
+		</h1>
+		<h1 className="nextMap">
+				Next Map: {data ? data["next"]["map"] : "Loading..."}
+		</h1>
 		
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
 
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
 
-export default function Data() {
-	
+function firstInterval(f, m, ...p) {
+  const h = setInterval(() => {
+    f.apply(null, p);
+  }, m);
+  f.apply(null, p);
 
+  return h;
 }
 
+export default function Data() {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    firstInterval(() => {
+      fetch(
+        "https://api.mozambiquehe.re/maprotation?auth=ee1be80b0af2c45ea77a855ee7fa2daa"
+      )
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    }, 2500);
+  }, []);
+
+  return data;
+}

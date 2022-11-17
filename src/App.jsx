4cc99deee6654686ba08a1bf0991c9c956bf5377
Snekxs <snekxs@gmail.com>
2022-11-17 13:02:20 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import React, { Component }  from 'react';
+import "./App.css";
+
+
+function App() {
+	return (
+	
+    <div className="App">
+      <div className="elements">Hello World</div>
+    </div>
+		
+  )
+}
+
+export default App

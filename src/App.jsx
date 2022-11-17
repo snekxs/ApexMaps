4005e426d9da206a4d9ca8c8047112e636c74c00
Snekxs <snekxs@gmail.com>
2022-11-17 13:04:37 -0700

    Added a singular word

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 	
     <div className="App">
-      <div className="elements">Hello World</div>
+      <div className="elements">Testing Word WOW</div>
     </div>
 		
   )
